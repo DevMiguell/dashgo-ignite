37b4c1fdfc8e499217dd3e54192a624d0fca9151
DevMiguell <buguel2000@gmail.com>
2021-05-25 09:43:39 -0300

    Componentezando a aplicação

---
+++ src/components/Header/Profile.tsx
@@ -0,0 +1,16 @@
+import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
+
+export function Profile() {
+  return (
+    <Flex align="center">
+      <Box mr="4" textAlign="right">
+        <Text>Fernando Miguel</Text>
+        <Text color="gray.300" fontSize="small">
+          buguel2000@gmail.com
+      </Text>
+      </Box>
+
+      <Avatar size="md" name="Fernando Miguel" src="https://avatars.githubusercontent.com/u/53983988?s=400&u=74f3c2c08f875bf0aa54d1461c6b697a1d3f4dff&v=4" />
+    </Flex>
+  )
+}
