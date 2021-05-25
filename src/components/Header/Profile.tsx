0a48995b0e269945bbf8558928da9ab8e1c031a5
DevMiguell <buguel2000@gmail.com>
2021-05-25 10:44:36 -0300

    Criando Responsividade e adicionando contexto

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,15 +1,20 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Fernando Miguel</Text>
-        <Text color="gray.300" fontSize="small">
-          buguel2000@gmail.com
-      </Text>
-      </Box>
-
+      { showProfileData &&
+        <Box mr="4" textAlign="right">
+          <Text>Fernando Miguel</Text>
+          <Text color="gray.300" fontSize="small">
+            buguel2000@gmail.com
+          </Text>
+        </Box>
+      }
       <Avatar size="md" name="Fernando Miguel" src="https://avatars.githubusercontent.com/u/53983988?s=400&u=74f3c2c08f875bf0aa54d1461c6b697a1d3f4dff&v=4" />
     </Flex>
   )
